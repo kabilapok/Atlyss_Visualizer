39bdf5c297488db12f557abf2d968088d37212d7
Retsuko.gg <88639923+kabilapok@users.noreply.github.com>
2025-09-03 18:10:49 -0400

    Move index.html to root

--- src/Scripts/dataLoader.js
+++ src/Scripts/dataLoader.js
@@ -7,11 +7,11 @@ export async function loadSkillData() {
             banditData,
             noviceData
         ] = await Promise.all([
-            fetch("data/skills/index.json").then((response) => response.json()),
-            fetch("data/skills/Fighter.json").then(response => response.json()),
-            fetch("data/skills/Mystic.json").then(response => response.json()),
-            fetch("data/skills/Bandit.json").then(response => response.json()),
-            fetch("data/skills/Novice.json").then(response => response.json())
+            fetch("src/data/skills/index.json").then((response) => response.json()),
+            fetch("src/data/skills/Fighter.json").then(response => response.json()),
+            fetch("src/data/skills/Mystic.json").then(response => response.json()),
+            fetch("src/data/skills/Bandit.json").then(response => response.json()),
+            fetch("src/data/skills/Novice.json").then(response => response.json())
         ]);
         return { iconData, fighterData, mysticData, banditData, noviceData };
     } catch (error) {
