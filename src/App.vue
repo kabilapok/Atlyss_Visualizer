0f9595163296cf82c6771797e9edbb0c2d2cbdd7
Retsuko.gg <88639923+kabilapok@users.noreply.github.com>
2025-10-04 12:15:18 -0400

    testing changes

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,14 @@ import skillLoadout from './components/skillLoadout.vue';
 
 import { ref } from 'vue';
 
-let currentIcon = ref('./images/misc/_ico_night.png');
+let currentIcon = ref('./images/misc/ico_night.png');
 
 function togglerIcon() {
   currentIcon.value =
-    currentIcon.value === './images/misc/_ico_night.png'
-      ? './images/misc/_ico_day.png'
-      : './images/misc/_ico_night.png';
+    currentIcon.value === '.public/images/misc/ico_night.png'
+      ? 'public/images/misc/ico_day.png'
+      : 'public/images/misc/ico_night.png';
 }
 </script>
 
-<style scoped>
-#theme-toggle {
-  background: none;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-}
-</style>
+<style scoped></style>
